3e5c42c6ecc2e7e7542e332a3d2dd67b59b2ffed
Mykola <mykolamalovanchuk@gmail.com>
2024-02-26 23:19:51 +0200

    implement MyHashMap

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,129 @@
 package core.basesyntax;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final int RESIZE_COEFFICIENT = 2;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    private Node<K, V>[] table;
+    private int size;
+
+    MyHashMap() {
+        this.table = new Node[DEFAULT_INITIAL_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        checkTableLength();
 
+        Node<K, V> newNode = new Node<>(generateHash(key), key, value, null);
+        int entityIndex = generateEntityIndex(newNode.hash);
+        Node<K, V> currentEntity = table[entityIndex];
+
+        if (currentEntity == null) {
+            table[entityIndex] = newNode;
+            size++;
+        } else {
+            while (true) {
+                if (Objects.equals(currentEntity.key, key)) {
+                    currentEntity.value = value;
+                    return;
+                }
+                if (currentEntity.next == null) {
+                    currentEntity.next = newNode;
+                    size++;
+                    return;
+                }
+                currentEntity = currentEntity.next;
+            }
+        }
+    }
+
+    private int generateHash(K key) {
+        int result = 31 * (key != null ? key.hashCode() : 0);
+        return result;
+    }
+
+    int generateEntityIndex(int hash) {
+        return Math.abs(hash & (table.length - 1));
+    }
+
+    private void checkTableLength() {
+        int threshold = (int) (table.length * DEFAULT_LOAD_FACTOR);
+        if (size >= threshold) {
+            copyEntitiesToNewTable();
+        }
+    }
+
+    private void copyEntitiesToNewTable() {
+        Node<K, V>[] currentTable = table;
+        Node<K, V>[] newTable = new Node[table.length * RESIZE_COEFFICIENT];
+
+        table = newTable;
+        size = 0;
+
+        for (Node<K, V> currentEntity : currentTable) {
+            while (currentEntity != null) {
+                put(currentEntity.key, currentEntity.value);
+                currentEntity = currentEntity.next;
+            }
+        }
     }
 
     @Override
     public V getValue(K key) {
+        int entityIndex = generateEntityIndex(generateHash(key));
+        Node<K, V> currentEntity = table[entityIndex];
+
+        while (currentEntity != null) {
+            if (Objects.equals(currentEntity.key, key)) {
+                return currentEntity.value;
+            }
+            currentEntity = currentEntity.next;
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
+    }
+
+    public void show() {
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                Node<K, V> entityOnCurrentIndex = table[i];
+
+                do {
+                    System.out.print("[ " + i + " ]   ");
+                    System.out.println("K: " + entityOnCurrentIndex.key
+                            + "    V: " + entityOnCurrentIndex.value);
+
+                    if (entityOnCurrentIndex.next != null) {
+                        entityOnCurrentIndex = entityOnCurrentIndex.next;
+                    } else {
+                        break;
+                    }
+                } while (true);
+            }
+        }
+        System.out.println("show end");
+    }
+
+    private class Node<K, V> {
+        private int hash;
+        private K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
     }
 }
---
+++ src/main/java/core/basesyntax/Test.java
@@ -0,0 +1,36 @@
+package core.basesyntax;
+
+public class Test {
+    public static void main(String[] args) {
+        MyHashMap<String, String> myHashMap = new MyHashMap<>();
+
+        myHashMap.put("0", "val=0");
+        myHashMap.put("1", "val=1");
+        myHashMap.put("100", "val=100");
+        myHashMap.put("2", "val=2");
+        myHashMap.put("200", "val=200");
+        myHashMap.put("3", "val=3");
+        myHashMap.put("300", "val=300");
+        myHashMap.put("4", "val=4");
+        myHashMap.put("400", "val=400");
+        myHashMap.put("5", "val=5");
+        myHashMap.put("500", "val=500");
+        myHashMap.put("6", "val=6");
+        myHashMap.put("600", "val=600");
+        myHashMap.put("7", "val=7");
+        myHashMap.put("700", "val=700");
+        myHashMap.put("8", "val=8");
+        myHashMap.put("800", "val=800");
+        myHashMap.put("9", "val=9");
+        myHashMap.put("900", "val=900");
+        myHashMap.put("901", "val=901");
+        myHashMap.put("902", "val=902");
+        myHashMap.put("904", "val=902");
+        myHashMap.put("905", "val=902");
+        myHashMap.put("906", "val=902");
+        myHashMap.put("907", "val=902");
+
+        myHashMap.show();
+
+    }
+}
